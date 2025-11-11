8995aee1359ed12c3d08452eec967669feb519c4
anuj tiwari <anshtiwari314@gmail.com>
2025-11-12 00:06:08 +0530

    comment eruda , fix content overflow issue on cues, cues scrollable on hi mobile also

--- src/reducers/healthManagmentReducer.ts
+++ src/reducers/healthManagmentReducer.ts
@@ -213,16 +213,34 @@ const initialCopilotLoadState= {
           header:'Follow-up Question',
           color:'blue',
           type:'regular-card',
-           data:[
-            {
-              id:'unique',
-              text:'Primary financial goals?'
-            },
-            {
-              id:'unique',
-              text:'Typical month financially?'
-            }
-          ],
+           "data": [
+               {
+                   "id": 'unique1',
+                   text:`Lorem Ipsum is simply dummy text of the printing 
+              and typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown printer 
+              took a galley of type and scrambled it to make a type 
+              specimen book. It has survived not only five centuries, but also 
+              the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset 
+              sheets containing Lorem Ipsum passages, and more recently with 
+              desktop publishing software like Aldus PageMaker including 
+              versions of Lorem Ipsum.`
+               },
+               {
+                   "id": 'unique2',
+                   text:`Lorem Ipsum is simply dummy text of the printing 
+              and typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown printer 
+              took a galley of type and scrambled it to make a type 
+              specimen book. It has survived not only five centuries, but also 
+              the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset 
+              sheets containing Lorem Ipsum passages, and more recently with 
+              desktop publishing software like Aldus PageMaker including 
+              versions of Lorem Ipsum.`
+               }
+           ],
         },
         {
           header:'Answer to "Why Term Plan?',
