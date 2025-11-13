7631fe0e2559ca8da2ccebf510c6c6718a45f8b2
roshan-jha-23 <jha062305@gmail.com>
2025-11-13 14:03:59 +0530

    testing the push?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['7cc57011447a.ngrok-free.app'], // Add your host here
+    allowedHosts: ['7cc57011abcd.ngrok-free.app','7cc57011447a.ngrok-free.app'], // Add your host here
   },
 })
