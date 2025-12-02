5e5efd8d16d73bfe545f32038b65a3406ba16f85
anuj tiwari <anshtiwari314@gmail.com>
2025-12-02 08:58:10 +0530

    ws now will disconnect at transition from mainpage to homepage

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 export const config =  {
     greet:'hello',
     // wsUrl:'wss://recruito.vitti.insure',
-     wsUrl:'http://localhost:8000',
+     wsUrl:'http://localhost:5000',
     // serverBaseUrl : 'https://wpv7kxos9g.execute-api.ap-south-1.amazonaws.com/test/recruito-upload-apis',
      serverBaseUrl : 'https://8104cef4eb8a.ngrok-free.app',
     //serverBaseUrl: 'https://eeabca1560e3.ngrok-free.app',
--- src/context/DataWrapper.tsx
+++ src/context/DataWrapper.tsx
@@ -125,8 +125,10 @@ export default function DataWrapper({ children }: { children: React.ReactNode })
 
   //const socketUrl = 'https://b18e4904236b.ngrok-free.app'
   // 500ms delay before connecting
-  const timer = setTimeout(() => {
-    const tempSocket = io(socketUrl)
+  
+  //const timer = setTimeout(() => {
+  
+  const tempSocket = io(socketUrl)
 
     function connected() {
       tempSocket.emit("connected", tempSocket.id);
@@ -146,7 +148,7 @@ export default function DataWrapper({ children }: { children: React.ReactNode })
     tempSocket.on('notifications',updateNotifications)
     setSocket(tempSocket)
     return () => {
-      timer && clearTimeout(timer)
+      //timer && clearTimeout(timer)
       if (tempSocket) {
         tempSocket.off("connect1", connected)
         tempSocket.off("disconnect1", disconnect)
@@ -155,7 +157,8 @@ export default function DataWrapper({ children }: { children: React.ReactNode })
         tempSocket.disconnect()
       }
     }
-  }, 500) 
+
+  //}, 500) 
 
 }, [])
 
