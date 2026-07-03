7458dd2017683e57575e5b3f609cb1077d272007
anshtiwari314 <anshtiwari314@gmail.com>
2026-07-03 23:43:35 +0530

    added service worker code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ij2b1l87l7"
+    "revision": "0.6ot5h7qln1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
